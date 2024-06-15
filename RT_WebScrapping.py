03021a05ad8d5bc3854f32f884e023f8e56f2165
EstebanGC <johan.gal.car@gmail.com>
2024-06-14 23:52:59 -0500

    Setting up some functions

--- RT_WebScrapping.py
+++ RT_WebScrapping.py
@@ -4,9 +4,15 @@ import requests
 from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup
 
+def wen_content_div(web_content, class_path):
+    web_content_div = web_content.find_all('div', {'class': class_path})
+    try:
+        spdns = web_content_div[0].find_all('span')
+
 
 def real_time_price(stock_code):
     url =
     try:
         r = requests.get(url)
-        web_content = 
+        web_content = BeautifulSoup(r.text, 'lxml')
+        texts = web_content_div(web_content, class_path)
