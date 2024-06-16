a78ac529316ea84194f638022ca03c0ff75ccb24
EstebanGC <johan.gal.car@gmail.com>
2024-06-15 19:34:44 -0500

    .

--- RT_WebScrapping.py
+++ RT_WebScrapping.py
@@ -8,11 +8,15 @@ def wen_content_div(web_content, class_path):
     web_content_div = web_content.find_all('div', {'class': class_path})
     try:
         spdns = web_content_div[0].find_all('span')
-
+        texts = [spans.get_text() for span in spans]
+    except IndexError:
+        texts = []
+    return texts
 
 def real_time_price(stock_code):
     url =
     try:
         r = requests.get(url)
         web_content = BeautifulSoup(r.text, 'lxml')
-        texts = web_content_div(web_content, class_path)
+        texts = web_content_div(web_content, 'class_path')
+    except ConnectionError
